105e877499407dd578e0adf02c4d7e0f4a907774
Duy Ngo <ntduy96@gmail.com>
2017-11-09 00:31:10 +0700

    create middleware for login and signup routes

--- app.js
+++ app.js
@@ -24,38 +24,54 @@ app.use(cookieParser());
 app.use(session({
   secret: 'secret',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: { maxAge: 1200000 } // 20*60*1000 = 1200000 => 20 minutes
 }));
+
+// only display login and signup when user is not logged in
+app.use(['/login', '/signup'], function isNotLoggedIn(req, res, next) {
+  if (!req.session.user) {
+    next();
+  } else {
+    res.redirect('/user');
+  }
+});
 
 //import connection to mysql
 var connection = require('./connection');
 
 app.get('/', function(req, res) {
   // req.session.username = 'ntduy96';
-  if (!req.session.user) {
-    res.redirect('/login');
-  } else {
-    res.redirect('/user');
-  }
+  // if (!req.session.user) {
+  res.redirect('/login');
+  // } else {
+  //   res.redirect('/user');
+  // }
 });
 
 //show login page
 app.get('/login', function(req, res) {
-  if (!req.session.user) {
-    // authenticate failed
-    if (req.query.status === 'failed') {
-      var message = 'Username or password is wrong, please try again!!:(';
-    }
-
-    // signed up successfully but need to log in
-    if (req.query.status === 'success' && req.query.action === 'signup') {
-      var message = 'You signed up successfully, please log in :)';
-    }
-
-    res.render('login', { 'message': message });
-  } else {
-    res.redirect('/user');
+  // if (!req.session.user) {
+  var message = '';
+  // authenticate failed
+  if (req.query.status === 'failed' && req.query.action === 'login') {
+    message = 'Username or password is wrong, please try again!!:(';
   }
+
+  // log out failed
+  if (req.query.status === 'failed' && req.query.action === 'signup') {
+    message = 'Please log in first!!:(';
+  }
+
+  // signed up successfully but need to log in
+  if (req.query.status === 'success' && req.query.action === 'signup') {
+    message = 'You signed up successfully, please log in :)';
+  }
+
+  res.render('login', { 'message': message });
+  // } else {
+  //   res.redirect('/user');
+  // }
 });
 
 //process data from login form submission
@@ -72,21 +88,24 @@ app.post('/login', function(req, res) {
       res.redirect('/user');
     } else {
       //Username or password is wrong, please try again!!:(
-      res.redirect('/login?status=failed');
+      res.redirect('/login?status=failed&action=login');
     }
   });
 });
 
 // show sign up page
 app.get('/signup', function(req, res) {
-  if (!req.session.user) {
-    if (req.query.status === 'failed') {
-      var message = 'Username or email is already used, please try another username or email!!:(';
-    }
-    res.render('signup', { 'message': message });
-  } else {
-    res.redirect('/user');
+  // if (!req.session.user) {
+  var message = '';
+  // email or username is already used
+  if (req.query.status === 'failed' && req.query.action === 'signup') {
+    message = 'Username or email is already used, please try another username or email!!:(';
   }
+
+  res.render('signup', { 'message': message });
+  // } else {
+  //   res.redirect('/user');
+  // }
 });
 // handle data form sign up
 app.post('/signup', function(req, res) {
@@ -101,7 +120,7 @@ app.post('/signup', function(req, res) {
   connection.query('INSERT INTO user SET ?', user, function(err, results) {
     if (err) {
       // Username or email is already used
-      res.redirect('/signup?status=failed');
+      res.redirect('/signup?status=failed&action=signup');
     } else {
       // user registered successfully :)
       res.redirect('/login?status=success&action=signup');
@@ -111,10 +130,7 @@ app.post('/signup', function(req, res) {
 
 // log out request handler
 app.post('/logout', function(req, res) {
-  if (!req.session.user) {
-    res.cookie('message', 'Please log in first!!:)', { path: '/login', maxAge: 3000 });
-    res.redirect('/login');
-  } else {
+  if (req.session.user) {
     req.session.destroy();
     res.redirect('/login');
   }
@@ -123,21 +139,23 @@ app.post('/logout', function(req, res) {
 // show user page
 app.get('/user', function(req, res) {
   if (req.session.user) {
-    // connection.query('SELECT * FROM course', function(err, results) {
-    //   var courses = results;
-    //   console.log(courses);
-    //   var queryStr = 'SELECT DISTINCT c.course_id, c.course_name, c.course_desc FROM enroll e INNER JOIN user u ON u.username = e.user INNER JOIN course c ON c.course_id = e.course WHERE u.username = ? ORDER BY c.course_id';
-    //   connection.query(queryStr, [req.session.user.username], function(err, results) {
-    //     console.log(results);
-    //     var enrolls = results || [];
-    //     res.render('user', {
-    //       message: 'Welcome ' + req.session.user.fname + ' ' + req.session.user.lname,
-    //       courses: courses,
-    //       enrolls: enrolls
-    //     });
-    //   });
+    /*
+    connection.query('SELECT * FROM course', function(err, results) {
+      var courses = results;
+      console.log(courses);
+      var queryStr = 'SELECT DISTINCT c.course_id, c.course_name, c.course_desc FROM enroll e INNER JOIN user u ON u.username = e.user INNER JOIN course c ON c.course_id = e.course WHERE u.username = ? ORDER BY c.course_id';
+      connection.query(queryStr, [req.session.user.username], function(err, results) {
+        console.log(results);
+        var enrolls = results || [];
+        res.render('user', {
+          message: 'Welcome ' + req.session.user.fname + ' ' + req.session.user.lname,
+          courses: courses,
+          enrolls: enrolls
+        });
+      });
 
-    // });
+    });
+    */
 
     connection.query('SELECT * FROM course; SELECT DISTINCT c.course_id, c.course_name, c.course_desc FROM enroll e INNER JOIN user u ON u.username = e.user INNER JOIN course c ON c.course_id = e.course WHERE u.username = ? ORDER BY c.course_id', [req.session.user.username], function(err, results) {
       res.render('user', {
